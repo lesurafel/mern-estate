87b7449925fe3bc5a90ec7cdbf791361b0005d69
Surafel Seyoum <lesurafel@gmail.com>
2023-12-13 17:46:44 -0800

    Create a middleware and a fuction to controll a posible error.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
+import { errorHandeler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
 	const hashedPassword = bcryptjs.hashSync(password, 10);
 	const newUser = new User({ username, email, password: hashedPassword });
-	await newUser.save(newUser);
-	res.status(201).json("Save sucessfully!");
+	try {
+		await newUser.save(newUser);
+		res.status(201).json("Save sucessfully!");
+	} catch (error) {
+		next(error);
+	}
+	// catch (error) {
+	// 	next(errorHandeler(550, "Error from function"));
+	// }
 };
--- api/index.js
+++ api/index.js
@@ -22,3 +22,14 @@ app.listen(3000, () => console.log("Server is running on port 3000!"));
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal server error";
+
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message
+	});
+});
